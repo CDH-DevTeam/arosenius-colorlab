28c0360e39e984e1650ad46285133c787cb6eb40
traustid <traustid@gmail.com>
2016-11-02 21:13:13 +0100

    Merged ColorCircle and ColorGraph into single class.

--- scripts/views/AppView.js
+++ scripts/views/AppView.js
@@ -5,7 +5,6 @@ var $ = require('jquery');
 var chroma = require('chroma-js');
 
 var AroseniusCollection = require('./../collections/AroseniusCollection');
-var ColorCircle = require('./ColorCircle');
 var ColorGraph = require('./ColorGraph');
 
 module.exports = Backbone.View.extend({
@@ -19,53 +18,22 @@ module.exports = Backbone.View.extend({
 			}
 		});
 
-//		this.drawPalette();
-
-//		this.createCircle();
-
 		this.createGraph();
 	},
 
-	drawPalette: function() {
-		this.canvas = $('.color-picker')[0];
-		this.ctx = this.canvas.getContext('2d');
-
-		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-		var hGrad = this.ctx.createLinearGradient(0, 0, this.canvas.width, 0);
-		hGrad.addColorStop(0 / 6, '#F00');
-		hGrad.addColorStop(1 / 6, '#FF0');
-		hGrad.addColorStop(2 / 6, '#0F0');
-		hGrad.addColorStop(3 / 6, '#0FF');
-		hGrad.addColorStop(4 / 6, '#00F');
-		hGrad.addColorStop(5 / 6, '#F0F');
-		hGrad.addColorStop(6 / 6, '#F00');
-
-		this.ctx.fillStyle = hGrad;
-		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-		var vGrad = this.ctx.createLinearGradient(0, 0, 0, this.canvas.height);
-
-//		vGrad.addColorStop(0, 'rgba(255,255,255,0)');
-//		vGrad.addColorStop(1, 'rgba(255,255,255,1)');
-
-
-		vGrad.addColorStop(0, 'rgba(150,150,150,0)');
-		vGrad.addColorStop(1, 'rgba(150,150,150,1)');
-
-		this.ctx.fillStyle = vGrad;
-		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-		$('.color-picker').click(_.bind(this.canvasClick, this));
+	events: {
+		'click .graph-type-links button': 'graphTypeButtonClick'
 	},
 
-	createCircle: function() {
-		this.colorCircle = new ColorCircle({
-			el: this.$el.find('.circle-wrapper')[0]
-		});
-		this.colorCircle.on('select', _.bind(function(event) {
-			this.collection.byColor(event.h, event.s, 50);
-		}, this));
+	graphTypeButtonClick: function(event) {
+		var type = $(event.currentTarget).data('type');
+
+		if (type == 'circular') {
+			this.colorGraph.setCircularGraph(true);
+		}
+		else {
+			this.colorGraph.setCircularGraph(false);
+		}
 	},
 
 	createGraph: function() {
@@ -75,25 +43,6 @@ module.exports = Backbone.View.extend({
 		this.colorGraph.on('select', _.bind(function(event) {
 			this.collection.byColor(event.h, event.s, 50);
 		}, this));
-	},
-
-	canvasClick: function(event) {
-		$('.canvas-marker').css({
-			top: event.offsetY-1,
-			left: event.offsetX-1
-		});
-		var point = this.ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data;
-
-		var hsv = chroma(point[0], point[1], point[2]).hsv();
-		hsv = {
-			h: !hsv[0] || hsv[0] == null || typeof hsv[0] === 'null' || Math.round(hsv[0]) == null ? 0 : Math.round(hsv[0]), 
-			s: !hsv[1] || hsv[1] == null || typeof hsv[1] === 'null' || Math.round(hsv[1]*100) == null ? 0 : Math.round(hsv[1]*100), 
-			v: !hsv[2] || hsv[2] == null || typeof hsv[2] === 'null' || Math.round(hsv[2]*100) == null ? 0 : Math.round(hsv[2]*100)
-		};
-
-		console.log('hue: '+hsv.h);
-		
-		this.collection.byColor(hsv.h, hsv.s, 50);
 	},
 
 	render: function() {
--- scripts/views/ColorGraph.js
+++ scripts/views/ColorGraph.js
@@ -7,6 +7,9 @@ var chroma = require('chroma-js');
 module.exports = Backbone.View.extend({
 	graphWidth: 1000,
 	graphHeight: 400,
+	
+	graphRadius: 250,
+
 	graphMargins: 20,
 
 	initialize: function() {
@@ -30,7 +33,7 @@ module.exports = Backbone.View.extend({
 		for (var hue = 0; hue<=360; hue++) {
 			var hueObj = {
 				hue: hue,
-				saturation: []	
+				saturation: []
 			};
 
 			for (var saturation = 0; saturation<=100; saturation++) {
@@ -40,51 +43,125 @@ module.exports = Backbone.View.extend({
 		}
 	},
 
+	circularGraph: false,
+
+	setCircularGraph: function(circular) {
+		this.circularGraph = circular;
+
+		this.renderGraph();
+		if (this.collection.length > 0) {
+			this.render();
+		}
+	},
+
 	renderGraph: function() {
-		this.xRange = d3.scaleLinear().range([this.graphMargins, this.graphWidth-(this.graphMargins*2)]).domain([0, 360]);
-		this.yRange = d3.scaleLinear().range([this.graphMargins, this.graphHeight-(this.graphMargins*2)]).domain([100, 0]);
+		d3.selectAll('svg#graphContainer > *').remove();
+
+		if (this.circularGraph) {
+			this.xRange = d3.scaleLinear().range([this.graphMargins, this.graphWidth-(this.graphMargins*2)]).domain([0, 360]);
+	//		this.yRange = d3.scaleLinear().range([this.graphMargins, this.graphRadius-(this.graphMargins*2)]).domain([0, 100]);
+			this.yRange = d3.scaleLinear().range([0, this.graphRadius]).domain([0, 100]);
+
+			this.$el.find('svg.graph-container').attr('width', (this.graphRadius*2)+this.graphMargins);
+			this.$el.find('svg.graph-container').attr('height', (this.graphRadius*2)+this.graphMargins);
+		}
+		else {
+			this.xRange = d3.scaleLinear().range([this.graphMargins, this.graphWidth-(this.graphMargins*2)]).domain([0, 360]);
+			this.yRange = d3.scaleLinear().range([this.graphMargins, this.graphHeight-(this.graphMargins*2)]).domain([100, 0]);
+
+			this.$el.find('svg.graph-container').attr('width', this.graphWidth+this.graphMargins);
+			this.$el.find('svg.graph-container').attr('height', this.graphHeight+this.graphMargins);
+		}
 
 		var view = this;
 
-		this.$el.find('svg.graph-container').attr('width', this.graphWidth+this.graphMargins);
-		this.$el.find('svg.graph-container').attr('height', this.graphHeight+this.graphMargins);
 
 		this.vis = d3.select('#graphContainer');
 
-		this.vis.selectAll('g.hue')
+		if (this.circularGraph) {
+			this.graphContainer = this.vis.append('g')
+				.attr('transform', function() {
+					return 'translate('+(view.graphRadius+(view.graphMargins/2))+', '+(view.graphRadius+(view.graphMargins/2))+')';
+				});
+		}
+		else {
+			this.graphContainer = this.vis;
+		}
+
+		this.graphContainer.selectAll('g.hue')
 			.data(this.baseData)
 			.enter()
 			.append('g')
 			.attr('class', 'hue')
+
+			.attr('transform', function(d) {
+				if (view.circularGraph) {
+					return 'rotate('+d.hue+')';
+				}
+			})
+
 			.each(function(data, hue) {
 				d3.select(this)
 					.selectAll('circle')
 					.data(data.saturation)
 					.enter()
 					.append('circle')
-					.attr('cx', function(d) {
-						return view.xRange(hue)
-					})
 					.attr('cy', function(d) {
 						return view.yRange(d)
 					})
-					.attr('r', 3)
+					.attr('cx', function(d) {
+						if (!view.circularGraph) {
+							return view.xRange(hue)
+						}
+					})
+					.attr('r', 4)
 					.attr('opacity', 0.8)
 					.attr('fill', function(d) {
-						return chroma(hue, (d/100), 1+((d/100)-0.6), 'hsv').hex();
+						return chroma(hue, (d/100), 0.4+(d/100), 'hsv').hex();
 					});
 			});
+/*
+		for (var i = 0; i<360; i += 1) {
 
+			for (var s = 1; s>=0.01; s -= 0.01) {
+				graphContainer.append('circle')
+					.datum({
+						h: i,
+						s: s
+					})
+					.attr('cx', function() {
+						return ((view.graphRadius-10-2)-((Math.abs(s-1)*120)))*Math.sin((i)*((2*Math.PI)/360));
+					})
+					.attr('cy', function() {
+						return ((view.graphRadius-10-2)-((Math.abs(s-1)*120)))*Math.cos((i+180)*((2*Math.PI)/360));
+					})
+					.attr('r', 1)
+					.attr('fill', function() {
+						return chroma(i, s, 1+(s-0.6), 'hsv').hex();
+					})
+					.on('click', function(event) {
+						view.trigger('select', event);
+					});
+			}
+		}
+*/
 	},
 
 	render: function() {
 		var view = this;
 
-		this.vis.selectAll('g.marker')
+		this.graphContainer.selectAll('g.marker')
 			.data(this.collection.models)
 			.enter()
 			.append('g')
 			.attr('class', 'marker')
+
+			.attr('transform', function(d) {
+				if (view.circularGraph) {
+					return 'rotate('+d.get('hue')+')';
+				}
+			})
+
 			.each(function(data, hue, more, evenmore) {
 				d3.select(this)
 					.selectAll('circle')
@@ -92,14 +169,16 @@ module.exports = Backbone.View.extend({
 					.enter()
 					.append('circle')
 					.attr('cx', function(d) {
-						return view.xRange(view.collection.at(hue).get('hue'))
+						if (!view.circularGraph) {
+							return view.xRange(view.collection.at(hue).get('hue'));
+						}
 					})
 					.attr('cy', function(d) {
 						return view.yRange(d)
 					})
 					.attr('r', 4)
 					.attr('fill', function(d) {
-						return chroma(view.collection.at(hue).get('hue'), (d/100), 1+((d/100)-0.6), 'hsv').hex();
+						return chroma(view.collection.at(hue).get('hue'), (d/100), 0.6+(d/100), 'hsv').hex();
 					})
 					.attr('style', 'cursor: pointer')
 					.attr('stroke', '#333')
